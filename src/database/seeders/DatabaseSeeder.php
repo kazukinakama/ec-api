bc065459bcc112fb2305361b1fefae0a04291b4d
kazukinakama <nakama.kazuki1215@gmail.com>
2022-05-31 00:58:29 +0900

    シーダー作成

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -2,23 +2,63 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Admin;
+use App\Models\Category;
+use App\Models\Image;
+use App\Models\Owner;
+use App\Models\Product;
+use App\Models\User;
+use App\Models\Variant;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
+    public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        Category::insert([
+            [
+                'id' => 1,
+                'name' => 'メンズ',
+            ],
+            [
+                'id' => 2,
+                'name' => 'レディース',
+            ],
+            [
+                'id' => 3,
+                'name' => 'キッズ',
+            ],
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $owners = Owner::factory(5)->create();
+
+        $owners->map(function ($owner) {
+            for ($i = 1; $i <= 10; $i++) {
+                Product::factory()
+                    ->create([
+                        'owner_id' => $owner->id,
+                        'category_id' => rand(1, 3)
+                    ]);
+            }
+        });
+
+        $owners->map(function ($owner) {
+            $owner->products->map(function ($product) {
+                Variant::factory(6)
+                    ->for($product)
+                    ->create();
+                for ($i = 1; $i <= 10; $i++) {
+                    Image::factory()
+                        ->for($product)
+                        ->create([
+                            'display_order' => $i,
+                            'filename' => 'image' . (string) rand(1, 7) . '.jpg',
+                        ]);
+                }
+            });
+        });
+
+        Admin::factory()->count(3)->create();
+        User::factory()->count(10)->create();
     }
 }
